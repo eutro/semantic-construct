158a93d5706c17888a44532d6cc4c9c93ccc2e48
eutro <git@eutro.dev>
2021-10-24 19:53:26 +0100

    Manual matrices time

--- src/externs.js
+++
@@ -1,6 +0,0 @@
-let DOMPoint = {};
-DOMPoint.matrixTransform = function(){};
-let DOMMatrix = {};
-DOMMatrix.translateSelf = function(){};
-DOMMatrix.scaleSelf = function(){};
-DOMMatrix.preMultiplySelf = function(){};
