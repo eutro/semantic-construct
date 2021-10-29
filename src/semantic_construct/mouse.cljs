efe0c31f693f894a02e53ead18af82bf5c064ae5
eutro <git@eutro.dev>
2021-10-29 18:49:30 +0200

    Is touch fixed yet?

--- src/semantic_construct/mouse.cljs
+++ src/semantic_construct/mouse.cljs
@@ -2,10 +2,13 @@
 
 (def mouse (atom [0 0]))
 (defn update-mouse [evt]
-  (reset! mouse [(.-pageX evt) (.-pageY evt)]))
+  (when evt (reset! mouse [(.-pageX evt) (.-pageY evt)])))
+
+(defn unwrap-touch [evt]
+  (-> evt .-touches first))
 
 (defonce initialized
   (do
     (set! js/document.onmousemove update-mouse)
-    (set! js/document.ontouchmove update-mouse)
+    (set! js/document.ontouchmove (comp update-mouse unwrap-touch))
     true))
